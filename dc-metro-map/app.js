842199d5bf17f895ea45ab42388fe6cf1d09825f
js-git-er <55640703+js-git-er@users.noreply.github.com>
2021-06-24 14:41:29 -0500

    Update app.js

--- dc-metro-map/app.js
+++ dc-metro-map/app.js
@@ -8,7 +8,6 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 
 var app = express();
-//var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
